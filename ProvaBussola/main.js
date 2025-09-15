8cb389089dac134c2ef4ee8f2fd38e742d4902ef
Daniel Barrionuevo Gomes <106540072+Cawlino@users.noreply.github.com>
2025-09-15 20:35:23 -0300

    Add files via upload

---
+++ ProvaBussola/main.js
@@ -0,0 +1,18 @@
+class Disciplina {
+    constructor(nome){
+        this.nome = nome
+    }
+}
+
+class Curso{
+    constructor(nome, cargaHoraria, dataInicio){
+        this.nome = nome;
+        this.cargaHoraria = cargaHoraria;
+        this.dataInicio = dataInicio;
+        this.disciplina = [];
+    }
+}
+
+addDisciplina(disciplina);{ 
+    this.disciplina.push(disciplina);
+}
